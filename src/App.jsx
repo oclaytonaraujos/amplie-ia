f507d5c616435fd060647205f6e3e7df10856860
Clayton Araujo <claytonaraujo.principal@.com>
2026-05-28 09:13:05 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown'
 import { MdOutlineEditNote } from "react-icons/md";
 import { FiSidebar } from "react-icons/fi";
 import { LuMessageCircle } from "react-icons/lu";
+import { HiSpeakerWave } from "react-icons/hi2";
 import { useAuth } from './context/AuthContext.jsx'
 import LoginScreen from './components/LoginScreen.jsx'
 import ResetPasswordScreen from './components/ResetPasswordScreen.jsx'
@@ -213,10 +214,7 @@ function SpeakButton({ text }) {
       `}
       style={{ minWidth: '24px', minHeight: '24px' }}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
-        <path d="M7 4a3 3 0 0 1 6 0v6a3 3 0 1 1-6 0V4Z" />
-        <path d="M5.5 9.643a.75.75 0 0 0-1.5 0V10c0 3.06 2.29 5.585 5.25 5.954V17.5h-1.5a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5h-1.5v-1.546A6.001 6.001 0 0 0 16 10v-.357a.75.75 0 0 0-1.5 0V10a4.5 4.5 0 0 1-9 0v-.357Z" />
-      </svg>
+      <HiSpeakerWave className="w-4 h-4" />
     </button>
   )
 }
